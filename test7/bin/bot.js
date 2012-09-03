423582af5a0091016500050e30b80077f8afea67
aph0x1 <aph0x1@gmail.com>
2012-09-03 00:36:18 -0300

    Update test7/bin/bot.js

--- test7/bin/bot.js
+++ test7/bin/bot.js
@@ -971,7 +971,7 @@
     }
 
     cervezaCommand.prototype.init = function() {
-      this.command = 'cerveza';
+      this.command = 'cookie';
       this.parseType = 'startsWith';
       return this.rankPrivelege = 'mod';
     };
